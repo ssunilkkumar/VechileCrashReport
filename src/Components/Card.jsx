46d10b79fe2ec5d01471a1a05a62c45fe7113118
Sunil Kumar <ssunil.uk@gmail.com>
2021-09-20 11:39:08 +0530

    animation added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,20 +8,31 @@ export const Card = ({vehicle_type_code1, vehicle_type_code2, crash_date, crash_
     return (
         <Link to={`/vehicle/${collision_id}`} style={{ textDecoration: 'none' }} >
             <div className={styles.card}>
-            <div className={styles.bgImage}>
-                <div className={styles.content}>
-                    <main>{vehicle_type_code1}</main>
-                    {vehicle_type_code2 && (
+                <div className={styles.bgImage}>
+                    <div className={styles.content}>
                         <div>
-                            <main>With</main>
-                            <main>{vehicle_type_code2}</main>
+                            <main>Vehicle 1</main>
+                            <div>{vehicle_type_code1}</div>
+
+                        {vehicle_type_code2 && (
+                                <div>
+                                    <main>Vechile 2</main>                      
+                                    <div>{vehicle_type_code2}</div>
+                                </div>
+                        )}
                         </div>
-                    )}
-                    <main>{`Date: `}{moment(crash_date).format("MM-DD-YYYY")}</main>
-                    <main>{`Time: ${crash_time}`}</main>
-                    {on_street_name && <main>{`Address: `}{on_street_name}</main>}
+                        <div>
+                            <main>{`Date: `}{moment(crash_date).format("MM-DD-YYYY")}</main>
+                            <main>{`Time: ${crash_time}`}</main>
+                        </div>
+                    </div>
+                    {/* {on_street_name && (
+                            <div>
+                                <main>Address</main>
+                                <div>{on_street_name}</div>
+                            </div>
+                        )} */}
                 </div>
-            </div>
             </div>
         </Link>
     )
